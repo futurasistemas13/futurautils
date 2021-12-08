5b5fddae20229fe4ee4b1fd250f0079cfdbeaa2c
matheusdacs <matheusdacs@gmail.com>
2021-12-08 16:24:13 -0300

    change serializable to dynamic serializable

--- src/Trait/JsonSerializable/JsonWithOutNull.php
+++ src/Trait/JsonSerializable/JsonWithOutNull.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Futuralibs\Futurautils\Trait\JsonSerializable;
 
+use Serializable;
+
 trait JsonWithOutNull
 {
     /**
@@ -10,22 +12,47 @@ trait JsonWithOutNull
      */
     public function jsonSerialize(): array
     {
-        $class = [];
+        $return     = array();
         $reflection = new \ReflectionClass(get_class($this));
 
-        foreach ($reflection->getParentClass()->getProperties() as $parent) {
-            $parent->setAccessible(true);
-            if ($parent->getValue($this) !== null) {
-                $class[$parent->getName()] = $parent->getValue($this);
-            }
+        $attributes = $reflection->getProperties();
+        while($reflection->getParentClass()){
+            $reflection = $reflection->getParentClass();
+            $attributes = array_merge_recursive($attributes, $reflection->getProperties());
         }
 
-        foreach ($reflection->getProperties() as $property) {
-            $property->setAccessible(true);
-            if ($property->getValue($this) !== null) {
-                $class[$property->getName()] = $property->getValue($this);
+        foreach($attributes  as $prop){
+            $attributeList = $prop->getAttributes();
+            if(count($attributeList) > 0){
+                foreach($attributeList as $attr){
+                    if(!($attr->getName() == Serializable::class)){
+                        continue;
+                    }
+                }
+            }else{
+                continue;
+            }
+
+            $prop->setAccessible(true);
+            if(is_array($prop->getValue($this))){
+                if(count($prop->getValue($this)) > 0){
+                    foreach($prop->getValue($this) as $item){
+                        if(is_object($item)){
+                            $return[$prop->getName()][]  =  $this->convert($item);  
+                        }else{
+                            $return[$prop->getName()] =  $prop->getValue($this);  
+                        }
+                    }
+                }else{
+                    $return[$prop->getName()] = array();
+                }
+
+            }elseif(is_object($prop->getValue($this))){
+                $return[$prop->getName()] = $this->convert($prop->getValue($this));
+            }else{
+                $return[$prop->getName()] =  $prop->getValue($this);
             }
         }
-        return $class;
+        return $return;
     }
 }
